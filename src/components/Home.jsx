650eeb11cca9ce0f07d9a5d80a4be509397af6f8
IvanPelusa <ivan_mj-bad@hotmail.com>
2020-05-19 19:35:23 -0500

    agrego footer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,7 +17,7 @@ class Home extends Component{
                 <h1 style={{textAlign:'center'}} >Celda DRAM</h1>
                 <Inicio />
                 
-                
+
                 <div>  
                 <ListGroup style={{width:"70%", paddingLeft:"30%", paddingBottom:"30px" , textAlign:'center'}} >
                     <ListGroupItem color="success">Chávez García Oscar Daniel </ListGroupItem>
@@ -26,6 +26,21 @@ class Home extends Component{
                     <ListGroupItem color="danger">Hernández García Ivan Alejandro</ListGroupItem>
                 </ListGroup>
                 </div>
+
+                                
+                <footer class="page-footer font-small blue pt-4" style={{textAlign:"center", paddingLeft:'30%'}}>
+                    <div class="container-fluid text-center text-md-left">
+                        <div class="row">
+                            <div class="col-md-6 mt-md-0 mt-3" style={{textAlign:"center", paddingRight:'0%'}}>
+                                <h5 class="text-uppercase">Dispositivos de almacenamiento de entrada y salida.</h5>
+                                <p>Proyecto final del profesor Carlos Saucedo</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footer-copyright text-center py-3" style={{textAlign:"center", paddingRight:'50%'}}>© 2020 Copyright:
+                        <p>UNAM_FI</p> 
+                    </div>
+                </footer>
             </div>
         )
     }
